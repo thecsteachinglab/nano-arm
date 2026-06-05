98be8ec5f8792e7b0571837dce05018c1f251097
thecsteachinglab <thecsteachinglab@gmail.com>
2026-06-05 11:52:20 +0100

    Update myapp.py

--- python/myapp.py
+++ python/myapp.py
@@ -1,10 +1,13 @@
 from roboarm import *
 
+#important: this number must match your arduino's COM port
 COM = "COM3"
 
+#initialise
 bot = Robo()
 bot.Initialise(COM)
 
+#send[part, direction, number of seconds]
 bot.Send(PART_BASE,DIR_1,2)
 bot.Send(PART_BASE,DIR_2,2)
 
@@ -20,7 +23,7 @@ bot.Send(PART_CLAW,DIR_2,2)
 bot.Send(PART_BASE,DIR_1,2)
 bot.Send(PART_BASE,DIR_2,2)
 
-
+#close the connection
 bot.Close()
 
 
